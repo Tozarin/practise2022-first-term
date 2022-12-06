483c48b86ec987d8684b0dddbe0498b0c2e05b8f
Startsev Matvey <89902951+Tozarin@users.noreply.github.com>
2022-12-06 22:11:17 +0300

    Make singleton classes

--- src/img_to_text_gen.py
+++ src/img_to_text_gen.py
@@ -1,57 +1,37 @@
 import torch
 from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
 
-itt_model = None
-itt_feature_extractor = None
-itt_tokenizer = None
-itt_device = None
-itt_inited = False
+class IttGenerator(object):
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(IttGenerator, cls).__new__(cls)
+        return cls.instance
 
-def init_itt():
-    global itt_model
-    global itt_feature_extractor
-    global itt_tokenizer
-    global itt_device
-    global itt_inited
+    model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
 
-    if itt_inited:
-        return itt_inited
+    def gen(self, image):
+        if image is None:
+            return None
 
-    try:
-        itt_model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-        itt_feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-        itt_tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+        max_length = 16
+        num_beams = 4
+        gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
 
-        itt_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        itt_model.to(itt_device)
-    except:
-        itt_inited = False
-        return False
+        if image.mode != "RGB":
+            image = image.convert(mode="RGB")
 
-    itt_inited = True
-    return itt_inited
+        images = [image]
 
-def gen_itt(image):
-    if not itt_inited:
-        return None
-    if image is None:
-        return None
+        pixel_values = self.feature_extractor(images=images, return_tensors="pt").pixel_values
+        pixel_values = pixel_values.to(self.device)
 
-    max_length = 16
-    num_beams = 4
-    gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+        output_ids = self.model.generate(pixel_values, **gen_kwargs)
 
-    if image.mode != "RGB":
-        image = image.convert(mode="RGB")
+        preds = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+        preds = [pred.strip() for pred in preds]
 
-    images = [image]
-
-    pixel_values = itt_feature_extractor(images=images, return_tensors="pt").pixel_values
-    pixel_values = pixel_values.to(itt_device)
-
-    output_ids = itt_model.generate(pixel_values, **gen_kwargs)
-
-    preds = itt_tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-    preds = [pred.strip() for pred in preds]
-
-    return preds[0]
+        return preds[0]
--- src/text_to_text_gen.py
+++ src/text_to_text_gen.py
@@ -1,29 +1,14 @@
 from transformers import pipeline, set_seed
 
-ttt_generator = None
-ttt_inited = False
+class TttGenerator(object):
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(TttGenerator, cls).__new__(cls)
+        return cls.instance
 
-def init_ttt():
-    global ttt_generator
-    global ttt_inited
+    ttt_generator = pipeline('text-generation', model='gpt2')
+    set_seed(42)
 
-    if ttt_inited:
-        return ttt_inited
-
-    try:
-        ttt_generator = pipeline('text-generation', model='gpt2')
-        set_seed(42)
-    except:
-        ttt_inited = False
-        return ttt_inited
-
-    ttt_inited = True
-    return ttt_inited
-
-def gen_ttt(base_text):
-    print(base_text)
-    if not ttt_inited:
-        return None
-
-    output = ttt_generator(base_text, max_length=512, num_return_sequences=1)
-    return output[0]['generated_text']
+    def gen(self, base_text):
+        output = self.ttt_generator(base_text, max_length=512, num_return_sequences=1)
+        return output[0]['generated_text']
