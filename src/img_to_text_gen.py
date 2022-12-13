f5866f5cf3704d5c111e32b17cbd8109d2d65286
Startsev Matvey <89902951+Tozarin@users.noreply.github.com>
2022-12-13 23:01:41 +0300

    Formats code

--- src/img_to_text_gen.py
+++ src/img_to_text_gen.py
@@ -1,23 +1,18 @@
 import torch
-from transformers import VisionEncoderDecoderModel, \
-    ViTFeatureExtractor, AutoTokenizer
+from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
 
 
 class IttGenerator(object):
     def __new__(cls):
-        if not hasattr(cls, 'instance'):
+        if not hasattr(cls, "instance"):
             cls.instance = super(IttGenerator, cls).__new__(cls)
         return cls.instance
 
     def __init__(self):
-        self.model = VisionEncoderDecoderModel\
-            .from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-        self.feature_extractor = ViTFeatureExtractor\
-            .from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-        self.tokenizer = AutoTokenizer\
-            .from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-        self.device = torch\
-            .device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+        self.feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+        self.tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(self.device)
 
     def gen(self, image, max_length, min_length, num_beams):
@@ -33,19 +28,14 @@ class IttGenerator(object):
 
         images = [image]
 
-        pixel_values = self.feature_extractor(
-            images=images,
-            return_tensors="pt").pixel_values
+        pixel_values = self.feature_extractor(images=images, return_tensors="pt").pixel_values
         pixel_values = pixel_values.to(self.device)
 
-        output_ids = self.model.generate(pixel_values,
-                                         max_length=max_length,
-                                         min_length=min_length,
-                                         num_beams=num_beams)
+        output_ids = self.model.generate(
+            pixel_values, max_length=max_length, min_length=min_length, num_beams=num_beams
+        )
 
-        preds = self.tokenizer.batch_decode(
-            output_ids,
-            skip_special_tokens=True)
+        preds = self.tokenizer.batch_decode(output_ids, skip_special_tokens=True)
         preds = [pred.strip() for pred in preds]
 
         return preds[0]
--- src/main.py
+++ src/main.py
@@ -9,11 +9,7 @@ def gen_text(image, max_length, min_length, num_beams):
     itt_generator = IttGenerator()
 
     try:
-        text = itt_generator.gen(
-            image=image,
-            max_length=max_length,
-            min_length=min_length,
-            num_beams=num_beams)
+        text = itt_generator.gen(image=image, max_length=max_length, min_length=min_length, num_beams=num_beams)
     except ValueError:
         return "Failed wille opening an image"
 
@@ -21,10 +17,7 @@ def gen_text(image, max_length, min_length, num_beams):
 
 
 def main():
-    st.set_page_config(
-        page_title='Funny descriptions generator',
-        page_icon=':skull:',
-        layout='wide')
+    st.set_page_config(page_title="Funny descriptions generator", page_icon=":skull:", layout="wide")
 
     num_beams = st.sidebar.slider("Num beams", 1, 16, 4)
     max_length = st.sidebar.slider("Max length", 1, 256, 128)
@@ -40,20 +33,16 @@ def main():
                 data = uploaded_file.getvalue()
                 image = Image.open(io.BytesIO(data))
             else:
-                image = Image.open('../images/cat.jpg')
+                image = Image.open("../images/cat.jpg")
 
             st.image(image)
 
         with right_column:
-            description = gen_text(
-                image=image,
-                max_length=max_length,
-                min_length=min_length,
-                num_beams=num_beams)
+            description = gen_text(image=image, max_length=max_length, min_length=min_length, num_beams=num_beams)
 
-            st.title('Description')
+            st.title("Description")
             st.write(description)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
